dece82bda5b46018b0c2552a719e54a2b3948a4f
nutchanon-c <nutchanon.char@gmail.com>
2022-11-09 12:22:08 +0000

    api server handle new upload

--- index.js
+++ index.js
@@ -1,9 +1,79 @@
 const { exec } = require("child_process");
+require("dotenv").config();
+const { v4: uuidv4 } = require("uuid");
+const AWS = require("aws-sdk");
+var fs = require("fs");
 var args = process.argv.slice(2);
 var command = args[0];
 const MASTER_KEY_PATH = "/home/ubuntu/abe/master_key";
 const PUB_KEY_PATH = "/home/ubuntu/abe/pub_key";
 console.log(args);
+
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const bodyParser = require("body-parser");
+const router = express.Router();
+const PORT = 3000;
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
+});
+
+// add router in the Express app.
+app.use("/", router);
+
+app.get("/", function (req, res, next) {
+  res.json({ msg: "Hello World" });
+});
+
+router.post("/new", (request, response) => {
+  //code to perform particular action.
+  //To access POST variable use req.body()methods.
+  console.log(request.body);
+  let set_id = uuidv4();
+  // TODO: ADD ALL DATA TO DATABASE
+  response.json({ msg: "Success", setID: set_id });
+});
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const uploadFile = async (localFilePath, cloudFilePath) => {
+  const filename = localFilePath;
+  const fileContent = fs.readFileSync(filename);
+
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `${cloudFilePath}`,
+    Body: fileContent,
+  };
+
+  // s3.upload(params, (err, data) => {
+  //   if (err) {
+  //     console.log(err);
+  //   }
+
+  //   return data.Location;
+  // });
+
+  const data = await s3.upload(params).promise(); // this line
+  console.log(`File uploaded successfully. ${data.Location}`);
+  return data.Location;
+};
+
+// uploadFile(
+//   "/home/ubuntu/steganography-server/test-files/testkey.txt",
+//   "keys/testkey.txt"
+// ).then((url) => console.log(url));
 
 const executeTerminalCommand = (command) => {
   exec(command, (error, stdout, stderr) => {
